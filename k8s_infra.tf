e5b26318c862805d84e2b475f0e6fa8efed260ca
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2016-12-28 13:32:08 +0000

    Add ansible foundations (WIP)

--- k8s_infra.tf
+++ k8s_infra.tf
@@ -184,10 +184,22 @@ resource "null_resource" "certificates" {
 
 resource "null_resource" "ansible-provision" {
   provisioner "local-exec" {
-    command = "echo '[kube-controllers]\n' > hosts/inventory"
+    command = "echo '[kube-controllers]\n' > ansible/inventory/hosts"
   }
 
   provisioner "local-exec" {
-    command = "echo \"${join("\n", formatlist("%s ansible_ssh_host=%s", google_compute_instance.controller.*.name, google_compute_instance.controller.*.network_interface.0.access_config.0.assigned_nat_ip))}\" >> hosts/inventory"
+    command = "echo \"${join("\n", formatlist("%s ansible_ssh_host=%s", google_compute_instance.controller.*.name, google_compute_instance.controller.*.network_interface.0.access_config.0.assigned_nat_ip))}\" >> ansible/inventory/hosts"
+  }
+
+  provisioner "local-exec" {
+    command = "echo '\n[kube-workers]\n' >> ansible/inventory/hosts"
+  }
+
+  provisioner "local-exec" {
+    command = "echo \"${join("\n", formatlist("%s ansible_ssh_host=%s", google_compute_instance.worker.*.name, google_compute_instance.worker.*.network_interface.0.access_config.0.assigned_nat_ip))}\" >> ansible/inventory/hosts"
+  }
+
+  provisioner "local-exec" {
+    command = "cd ansible; ansible-playbook -i inventory/hosts -u danielbryant --private-key /Users/danielbryant/.ssh/gcloud-id-2 bootstrap.yaml"
   }
 }
