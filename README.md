7b6ab4e9d3bea1c5c4a0274c9388473bb1fa800a
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2017-01-07 12:12:12 +0000

    Refactor directory structure to separate AWS and GCP

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # kubernetes-terraform-play
+
+Experimental [Terraform](https://www.terraform.io/) and [Ansible](https://www.ansible.com/)
+version of Kelsey Hightower's [Kubernetes the Hard Way](https://github.com/kelseyhightower/kubernetes-the-hard-way)
--- aws/certificates.tf
+++ aws/certificates.tf
@@ -1,5 +1,5 @@
 data "template_file" "certificates" {
-  template = "${file("../template/kubernetes-csr.json.tpl")}"
+  template = "${file("../terraform-templates/kubernetes-csr.json.tpl")}"
   vars {
     controller0_ip = "${aws_instance.controller.0.public_ip}"
     controller1_ip = "${aws_instance.controller.1.public_ip}"
@@ -19,6 +19,6 @@ resource "null_resource" "certificates" {
     command = "mkdir cert-authority; echo '${data.template_file.certificates.rendered}' > cert-authority/kubernetes-csr.json"
   }
   provisioner "local-exec" {
-    command = "cd cert-authority; cfssl gencert -initca ../../template/ca-csr.json | cfssljson -bare ca; cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=../../template/ca-config.json -profile=kubernetes kubernetes-csr.json | cfssljson -bare kubernetes"
+    command = "cd cert-authority; cfssl gencert -initca ../../terraform-templates/ca-csr.json | cfssljson -bare ca; cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=../../terraform-templates/ca-config.json -profile=kubernetes kubernetes-csr.json | cfssljson -bare kubernetes"
   }
 }
