bef90319ad22ac20d80dc1c942e848c80735b1dd
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2017-01-07 12:15:19 +0000

    Fix issues with dir refactor

--- gcp/ansible_provisioning.tf
+++ gcp/ansible_provisioning.tf
@@ -1,22 +1,22 @@
 resource "null_resource" "ansible-provision" {
   depends_on = ["null_resource.certificates"]
   provisioner "local-exec" {
-    command = "echo '[kube-controllers]\n' > ansible/inventory/hosts"
+    command = "echo '[kube-controllers]\n' > inventory/hosts"
   }
 
   provisioner "local-exec" {
-    command = "echo \"${join("\n", formatlist("%s ansible_ssh_host=%s", google_compute_instance.controller.*.name, google_compute_instance.controller.*.network_interface.0.access_config.0.assigned_nat_ip))}\" >> ansible/inventory/hosts"
+    command = "echo \"${join("\n", formatlist("%s ansible_ssh_host=%s", google_compute_instance.controller.*.name, google_compute_instance.controller.*.network_interface.0.access_config.0.assigned_nat_ip))}\" >> inventory/hosts"
   }
 
   provisioner "local-exec" {
-    command = "echo '\n[kube-workers]\n' >> ansible/inventory/hosts"
+    command = "echo '\n[kube-workers]\n' >> inventory/hosts"
   }
 
   provisioner "local-exec" {
-    command = "echo \"${join("\n", formatlist("%s ansible_ssh_host=%s", google_compute_instance.worker.*.name, google_compute_instance.worker.*.network_interface.0.access_config.0.assigned_nat_ip))}\" >> ansible/inventory/hosts"
+    command = "echo \"${join("\n", formatlist("%s ansible_ssh_host=%s", google_compute_instance.worker.*.name, google_compute_instance.worker.*.network_interface.0.access_config.0.assigned_nat_ip))}\" >> inventory/hosts"
   }
 
   provisioner "local-exec" {
-    command = "cd ansible; ansible-playbook -u ${var.instance_ssh_username} --private-key ${var.instance_private_key} bootstrap.yaml"
+    command = "ansible-playbook -i inventory/hosts -u ${var.instance_ssh_username} --private-key ${var.instance_private_key} ../ansible/bootstrap.yaml"
   }
 }
