c99b3b1d5661d4495f583d72038b7c083cb85f06
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2017-01-07 10:54:07 +0000

    Additional work on Ansible provisioning (WIP)

---
+++ aws/ansible_provisioning.tf
@@ -0,0 +1,7 @@
+resource "null_resource" "ansible-provision" {
+  depends_on = ["null_resource.certificates"]
+
+  provisioner "local-exec" {
+    command = "ansible-playbook -i inventory -u ${var.instance_ssh_username} --private-key ${var.instance_private_key_file} ../ansible/bootstrap.yaml"
+  }
+}
---
+++ aws/certificates.tf
@@ -0,0 +1,24 @@
+data "template_file" "certificates" {
+  template = "${file("../template/kubernetes-csr.json.tpl")}"
+  vars {
+    controller0_ip = "${aws_instance.controller.0.public_ip}"
+    controller1_ip = "${aws_instance.controller.1.public_ip}"
+    controller2_ip = "${aws_instance.controller.0.public_ip}"
+    worker0_ip = "${aws_instance.worker.0.public_ip}"
+    worker1_ip = "${aws_instance.worker.1.public_ip}"
+    worker2_ip = "${aws_instance.worker.2.public_ip}"
+    kubernetes_public_address = "${aws_elb.kubernetes.dns_name}"
+  }
+}
+
+resource "null_resource" "certificates" {
+  triggers {
+    template_rendered = "${data.template_file.certificates.rendered}"
+  }
+  provisioner "local-exec" {
+    command = "mkdir cert-authority; echo '${data.template_file.certificates.rendered}' > cert-authority/kubernetes-csr.json"
+  }
+  provisioner "local-exec" {
+    command = "cd cert-authority; cfssl gencert -initca ../../template/ca-csr.json | cfssljson -bare ca; cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=../../template/ca-config.json -profile=kubernetes kubernetes-csr.json | cfssljson -bare kubernetes"
+  }
+}
