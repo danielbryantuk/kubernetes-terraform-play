5859556a52e1efad6bf9198d757203dc69f2cf10
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2017-01-07 08:35:49 +0000

    Add workers

---
+++ aws/workers.tf
@@ -0,0 +1,27 @@
+variable "worker_ips" {
+  default = {
+    "0" = "10.240.0.20"
+    "1" = "10.240.0.21"
+    "2" = "10.240.0.22"
+    }
+}
+
+resource "aws_instance" "worker" {
+  count = "3"
+  ami = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.small"
+
+  iam_instance_profile = "${aws_iam_instance_profile.kubernetes.id}"
+  key_name = "${aws_key_pair.kubernetes.key_name}"
+
+  vpc_security_group_ids = ["${aws_security_group.kubernetes.id}"]
+  subnet_id = "${aws_subnet.kubernetes.id}"
+  source_dest_check = false
+
+  associate_public_ip_address = true
+  private_ip = "${lookup(var.worker_ips, count.index)}"
+
+  tags {
+    Name = "worker${count.index}"
+  }
+}
