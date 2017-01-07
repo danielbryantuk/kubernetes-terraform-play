c0e7b189baee3c8381cc41c5c6457a63fb08dbd9
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2017-01-07 10:51:35 +0000

    Fix incorrect Ubuntu image version

--- aws/controllers.tf
+++ aws/controllers.tf
@@ -2,7 +2,7 @@ data "aws_ami" "ubuntu" {
   most_recent = true
   filter {
     name = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
   }
   filter {
     name = "virtualization-type"
