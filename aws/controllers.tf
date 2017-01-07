e8d3313ca7baacdbda9a32fb7da935e8f34cc98e
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2017-01-07 10:52:26 +0000

    Extract ami data source to external file

--- aws/controllers.tf
+++ aws/controllers.tf
@@ -1,16 +1,3 @@
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  filter {
-    name = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
-  }
-  filter {
-    name = "virtualization-type"
-    values = ["hvm"]
-  }
-  owners = ["099720109477"] # Canonical
-}
-
 variable "controller_ips" {
   default = {
     "0" = "10.240.0.10"
