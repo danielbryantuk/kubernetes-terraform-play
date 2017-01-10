01ef0c7538ef763ac3b8eba1cf74cfc83ade0e77
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2017-01-10 19:30:29 +0000

    Include new modules (WIP)

--- gcp-modules/main.tf
+++ gcp-modules/main.tf
@@ -3,3 +3,18 @@ provider "google" {
   project     = "${var.project_name}"
   region      = "${var.region}"
 }
+
+module "k8s-controller" {
+  source = "./modules/k8s-controller"
+  name = "k8s-controller"
+}
+
+module "k8s-worker" {
+  source = "./modules/k8s-worker"
+  name = "k8s-worker"
+}
+
+module "staging" {
+  source = "./modules/staging"
+  name = "staging"
+}
