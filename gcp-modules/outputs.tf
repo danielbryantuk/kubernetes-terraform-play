ee1ae940a8047242f6869b4c95be6e69f2ae2919
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2017-01-12 12:08:36 +0000

    Corrected root module outputs

--- gcp-modules/outputs.tf
+++ gcp-modules/outputs.tf
@@ -1,3 +1,3 @@
-/*output "kubernete_cluster_ip" {
-  value = "${module.staging.google_compute_address.kubernetes.address}"
-}*/
+output "kubernete_cluster_ip" {
+  value = "${module.staging.k8s-network-address}"
+}
